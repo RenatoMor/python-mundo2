4f34d47ce05e07e5c601625b51ea65e53b507d1a
Renato Mor <renato.moreira@uni9.edu.br>
2026-05-12 13:26:55 -0300

    versao_1.1.0 - DF37 conversor de base numerica

--- src/resolver/desafio_36.py
+++ src/resolver/desafio_36.py
@@ -1,30 +1,25 @@
-# * Escreva um program para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# $  Escreva um program para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 
-# $ Calcule o valor da prestação mensal, sabendo que pode exceder 30% do salário ou então o empréstimo será negado.
+# $  $ Calcule o valor da prestação mensal, sabendo que pode exceder 30% do salário ou então o empréstimo será negado.
 
-# Valor do empréstimo
-# Valor da casa
-# Salario do comprador
+# $ Valor do empréstimo
+# $ Valor da casa
+# $ Salario do comprador
 
-# Quantidade de anos para pagar (meses)
+# $ Quantidade de anos para pagar (meses)
 
+casa = float(input("Informe o valor da casa: R$ "))
+salario = float(input("Informe o seu salário: R$ "))
+anos = int(input("Em quantos anos quer pagar o imóvel: "))
+minimo = salario * 30 / 100
+prestacao = casa / (anos * 12)
 
-valor_da_casa = 0
-valor_do_emprestimo = 0
-salario_do_comprador = 0
-
-valor_da_casa = float(input("Informe o valor da casa: R$ "))
-salario_do_comprador = float(input("Informe o seu salário: R$ "))
-quantidade_de_anos_para_pagar = int(input("Em quantos anos quer pagar o imóvel: "))
-
-percentual_do_salario = salario_do_comprador * 0.3
-print(f"30% do seu salário é R$ {percentual_do_salario:.2f}")
-valor_prestacao_mensal = valor_da_casa / (quantidade_de_anos_para_pagar * 12)
-print(f"O valor da prestação mensal é de R$ {valor_prestacao_mensal:.2f}")
+print(f"Limite: 30% do seu salário é R$ {minimo:.2f}")
+print(f"O valor da prestação mensal é de R$ {prestacao:.2f}")
 
 
-if valor_prestacao_mensal <= percentual_do_salario:
+if prestacao <= minimo:
     print("Empréstimo aprovado!")
 
-elif valor_prestacao_mensal > percentual_do_salario:
+else:
     print("Empréstimo negado!")
